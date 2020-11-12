bbfd1f589d56b7868f75695bb2ef31c96fa63cb1
Elijah Desktop <56444910+EaStory@users.noreply.github.com>
2020-11-12 15:08:09 -0500

    Update Lab8-Motion-Button-Lantern-Elijah-Story.ino

--- Lab8-Motion-Button-Lantern-Elijah-Story/Lab8-Motion-Button-Lantern-Elijah-Story.ino
+++ Lab8-Motion-Button-Lantern-Elijah-Story/Lab8-Motion-Button-Lantern-Elijah-Story.ino
@@ -2,19 +2,18 @@
 //11-6-2020
 //NMD211 Lab8
 
-int motionLed = 13;             // pin for LED controled by sensor
-int buttonLed = 12;             // pin for LED controled by sensor and button
+int lanternLed = 11;            // pin for lantern LED
 int sensorInput = 8;            // input pin (for PIR sensor)
 int buttonInput = 7;            // input for button
 
 int val = 0;                    // variable for reading the pin status
 boolean state = false;          // if the sensor input is "on"
-int button = 0;
-int buttonLast = 0;
+int button = 0;                 //if the button is pressed
+int buttonLast = 0;             //stores what the button was last pressed
+int level;
 
 void setup() {
-  pinMode(motionLed, OUTPUT);   // declare LED as output
-  pinMode(buttonLed, OUTPUT);   // declare LED as output
+  pinMode(lanternLed, OUTPUT);   // declare LED as output
   pinMode(sensorInput, INPUT);  // declare sensor as input
   pinMode(buttonInput, INPUT);  // declare sensor as input
 
@@ -22,30 +21,28 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(state);
-  button = digitalRead(buttonInput);
-  if (button == HIGH) {
-    if(button != buttonLast){
-      state = !state;
+  Serial.println(state);              //prints the "on/off" sensor status
+  button = digitalRead(buttonInput);  //gets button input
+  if (button == HIGH) {               //if the button is pressed
+    if(button != buttonLast){         //if the button is opposite last status
+      state = !state;                 //changes the "state" of state
     }
   }
-  buttonLast = button;
+  buttonLast = button;                //updates the last button status
 
   if (!state) {
-    digitalWrite(buttonLed, LOW);  // turn button LED OFF
+    digitalWrite(lanternLed, 0);  // turn button LED OFF
   }
 
   val = digitalRead(sensorInput);  // read input value
 
   if (val == HIGH) { // check if the input is HIGH
-    digitalWrite(motionLed, HIGH);  // turn sensor LED ON
     if (state) {
-      digitalWrite(buttonLed, HIGH);  // turn button LED ON
+      level = random(100,255);
+      digitalWrite(lanternLed, level);  // turn lantern LED ON
     }
 
   } else {
-    digitalWrite(motionLed, LOW); // turn LED OFF
-    digitalWrite(buttonLed, LOW);  // turn button LED OFF
-
+    digitalWrite(lanternLed, 0);  // turn lantern LED OFF
   }
 }
