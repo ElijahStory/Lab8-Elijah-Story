984d586a0ec48553d8da2e62d59f5c8252159169
Elijah Desktop <56444910+EaStory@users.noreply.github.com>
2020-11-18 18:29:23 -0500

    Update Lab8-Motion-Button-Lantern-Elijah-Story.ino

--- Lab8-Motion-Button-Lantern-Elijah-Story/Lab8-Motion-Button-Lantern-Elijah-Story.ino
+++ Lab8-Motion-Button-Lantern-Elijah-Story/Lab8-Motion-Button-Lantern-Elijah-Story.ino
@@ -2,23 +2,30 @@
 //11-6-2020
 //NMD211 Lab8
 
+//This is the current state of my lantern. 
+//I have a button set up to manually turn the lantern on/off. 
+//When the lantern is on, it has a flickering effect. 
+//While it is on, the motion sensor detects somebody, 
+//the light turns off for 3 seconds. After the time has passed, 
+//it turns back on and flickers.
+
 int lanternLed = 11;            // pin for lantern LED
 int sensorInput = 8;            // input pin (for PIR sensor)
 int buttonInput = 7;            // input for button
 
-int val = 0;                    // variable for reading the pin status
+int val = 0;                    // variable for reading the PIR status
 boolean state = false;          // if the sensor input is "on"
 int button = 0;                 // if the button is pressed
 int buttonLast = 0;             // stores what the button was last pressed
 int level = 100;                // stores the random level of power for LED
-double curTime;
-double pastTime = 0;
-int randDelay;
+double curTime;                 // this will hold the current time
+double pastTime = 0;            // this will hold the last time that was collected
+int randDelay;                  //this is the delay between LED levels
 
 void setup() {
   pinMode(lanternLed, OUTPUT);  // declare LED as output
   pinMode(sensorInput, INPUT);  // declare sensor as input
-  pinMode(buttonInput, INPUT);  // declare sensor as input
+  pinMode(buttonInput, INPUT);  // declare button as input
 
   Serial.begin(9600);
 }
@@ -38,20 +45,19 @@ void loop() {
   }
 
   val = digitalRead(sensorInput);  // read input value
-  Serial.println(val);
 
-  if (val == LOW) { // check if the input is HIGH
-    if (state) {
-      analogWrite(lanternLed, level);  // turn lantern LED ON
-      curTime = millis();
-      if(curTime - pastTime >= randDelay){
-        level = random(50,255);
-        randDelay = random(50,200);
-        pastTime = curTime;
+  if (val == LOW) { // check if the input is LOW "PIR sees nothing"
+    if (state) {                        //if the button is "on"
+      analogWrite(lanternLed, level);   // turn lantern LED ON
+      curTime = millis();               //get the current time
+      if(curTime - pastTime >= randDelay){  //if the time that has passed is greater delay
+        level = random(50,255);             //pick a new power level for LED
+        randDelay = random(50,200);         //pick new delay time
+        pastTime = curTime;                 //update the past time
       }
     }
 
-  } else {
+  } else {//PIR sees something
     digitalWrite(lanternLed, 0);  // turn lantern LED OFF
   }
 }
